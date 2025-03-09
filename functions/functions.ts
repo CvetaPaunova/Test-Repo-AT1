b66ef5e767b430ad8d40a076f80eb981df3c5389
Cveta Paunova (Crossroad) <cpaunova@crossroad.bg>
2025-03-09 16:57:54 +0200

    First commit

---
+++ functions/functions.ts
@@ -0,0 +1,9 @@
+function greet(): void {
+  console.log("Hello to functions!");
+}
+greet();
+function multiplyNums(numberOne: number, numberTwo: number): number {
+  return numberTwo * numberOne;
+}
+
+multiplyNums(2, 5);
